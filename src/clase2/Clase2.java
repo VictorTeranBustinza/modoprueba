672a1b2bc0bf032e578220ad29027d405fc63c17
l03m13@SURCO.COM <l03m13@SURCO.COM>
2020-02-07 22:21:31 -0500

    primer commit

---
+++ src/clase2/Clase2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author l03m13
+ */
+public class Clase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
